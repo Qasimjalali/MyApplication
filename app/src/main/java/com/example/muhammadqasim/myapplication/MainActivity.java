153f18b110cff1122bb6c2b40daac18594085872
Muhammad Qasim <m.qasim5595@gmail.com>
2017-09-22 02:52:46 +0500

    test

---
+++ app/src/main/java/com/example/muhammadqasim/myapplication/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.muhammadqasim.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button hospital=(Button)findViewById(R.id.Hospital);
+        Button resturant =(Button)findViewById(R.id.Resturent);
+        Button schools=(Button)findViewById(R.id.School);
+        Button atm=(Button)findViewById(R.id.Atm);
+
+        hospital.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+        resturant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+        schools.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+        atm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
